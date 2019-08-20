22ee287fabae6dcb4cf8f2853da5ea57a051a19d
lookpearsw <supajeew@gmail.com>
2019-08-20 17:42:04 +0700

    Use the data class for the nickname in the TextView

--- app/src/main/java/com/gmail/supajeew/aboutme/MainActivity.kt
+++ app/src/main/java/com/gmail/supajeew/aboutme/MainActivity.kt
@@ -13,31 +13,44 @@ import com.gmail.supajeew.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Supajee Werasatean")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.doneButton.setOnClickListener {
-            addNickname(it)
+        binding.apply {
+            binding.doneButton.setOnClickListener {
+                addNickname(it)
+            }
+            binding.nicknameText.setOnClickListener {
+                updateNickname(it)
+            }
         }
-        binding.nicknameText.setOnClickListener {
-            updateNickname(it)
-        }
+        binding.myName = myName
     }
 
     private fun addNickname(view: View) {
-        val editText = binding.nicknameEdit
-        val nicknameTextView = binding.nicknameText
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            nicknameEdit.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+            doneButton.visibility = View.GONE
 
-        val doneButton = binding.doneButton
-       doneButton.visibility = View.GONE
+            invalidateAll()
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+//        val editText = binding.nicknameEdit
+//        val nicknameTextView = binding.nicknameText
+//
+//        editText.visibility = View.GONE
+//        nicknameTextView.visibility = View.VISIBLE
+//
+//        val doneButton = binding.doneButton
+//        doneButton.visibility = View.GONE
 
-//        editText.requestFocus()
+
 //        hide keyboard
-        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+
     }
 
     private fun updateNickname(view: View) {
---
+++ app/src/main/java/com/gmail/supajeew/aboutme/MyName.kt
@@ -0,0 +1,3 @@
+package com.gmail.supajeew.aboutme
+
+data class MyName(var name: String = "", var nickname: String = "")
